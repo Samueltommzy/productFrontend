23e49a94b0367c1593f234249528870153f599d8
Samueloluwatomi <samueloluwatommzy@gmail.com>
2019-02-19 11:57:31 +0100

    Angular Project scaffold

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ProductsComponent } from './products/products.component';
+
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
