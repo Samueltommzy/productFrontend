a02a88198e17c145a662055c724a5422154a439d
Samueloluwatomi <samueloluwatommzy@gmail.com>
2019-02-20 13:57:25 +0100

    update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/productfrontend'));
 app.listen(process.env.PORT||3000,(err)=>{
     if(err)console.log(err);
     console.log("app listening on 3000");
